47876a3f4588c7461db54052c18512474b75001e
Patrick <Patrick@gdi2290.com>
2012-12-12 19:22:32 -0800

    update

--- RubyCalculator.rb
+++ RubyCalculator.rb
@@ -2,60 +2,59 @@
 #   Basic Ruby Calculator v0.01
 #          by gdi2290
 #
-#    Ruby version: Ruby 1.9.3 p0
-#   
+#   Ruby version: Ruby 1.9.3 p327
+#
 
-# Uhmmm.... lots of things going on here :P
-def crunchNumbers(firstNumber= 0, secondNumber= 0, operatorType=3)
-  if operatorType == "Subtract" or
-     operatorType == "subtract" or
-     operatorType == "Minus"    or
-     operatorType == "minus"    or
+def crunch_numbers(first_number= 0, second_number= 0, operatorType=3)
+  if operatorType == 'Subtract' or
+     operatorType == 'subtract' or
+     operatorType == 'Minus'    or
+     operatorType == 'minus'    or
      operatorType == "-"
-     answer = firstNumber - secondNumber
-     sign = "-"
-     stringSign = "subtract from"
-  elsif operatorType == "Divide" or
-        operatorType == "divide" or 
-        operatorType == "/"
-     answer = firstNumber / secondNumber
-     sign = "/"
-     stringSign = "divide from"
-  else operatorType == "Add"  or
-       operatorType == "add"  or
-       operatorType == "Plus" or
-       operatorType == "plus" or
-       operatorType == "+"  
-     answer = firstNumber + secondNumber
-     sign = "+"
-     stringSign = "add to"
+     answer = first_number - second_number
+     sign = '-'
+     string_sign = "subtract from"
+  elsif operatorType == 'Divide' or
+        operatorType == 'divide' or
+        operatorType == "\/"
+     answer = first_number / second_number
+     sign = '/'
+     string_sign = 'divide from'
+  else operatorType == 'Add'  or
+       operatorType == 'add'  or
+       operatorType == 'Plus' or
+       operatorType == 'plus' or
+       operatorType == '+'
+     answer = first_number + second_number
+     sign = '+'
+     string_sign = "add to"
    end
-  return answer, sign, stringSign
+  return answer, sign, string_sign
 end
 
 # Welcome message
-puts "Welcome to my awesome calculator made by gdi2290 \n" 
+puts "Welcome to my Basic Calculator\n"
 
-# User selects an Operator 
-print "Please select your math operator: Add, Subtrack, or Divide? "
+# User selects an Operator
+puts "Please select your Math operator: Add, Subtrack, or Divide? "
 
 # Converts the String operator to a Variable
-operator = gets.chomp.to_s
-stringSign = crunchNumbers(0,0,operator) # I need another way to grab a default operator
+user_imput = gets.chomp.to_s
+string_sign = crunch_numbers(0,0,user_imput) # I need another way to grab a default operator
 
 # User selects his or her first number
 print "What is the first number? "
-firstNumber = gets.chomp.to_f
+first_number = gets.chomp.to_f
 
 # User selects his or her second number
-print "What is the second number you want to #{stringSign[2]} #{firstNumber}? "
-secondNumber = gets.chomp.to_f
+print "What is the second number you want to #{string_sign[2]} #{first_number}? "
+second_number = gets.chomp.to_f
 
 # Calculates the answer
-answer = crunchNumbers(firstNumber, secondNumber, operator)
+answer = crunch_numbers(first_number, second_number, user_imput)
 
 # Prints out the answer
-puts "#{firstNumber} #{answer[1]} #{secondNumber} = #{answer[0]} \n\n"
+puts "#{first_number} #{answer[1]} #{second_number} = #{answer[0]} \n\n"
 puts "The answer is #{answer[0]}"
 
 
