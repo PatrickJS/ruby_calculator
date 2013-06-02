bb650ac5f40d32f92a46d4fe6a2fdc7da1be287a
Patrick <github@gdi2290.com>
2013-06-01 23:42:36 -0600

    update for MRI 1.9+ support

--- RubyCalculator.rb
+++ RubyCalculator.rb
@@ -8,11 +8,11 @@
 class Calculator
   def initialize
     @cal={
-      add:      -> (a,b) { a + b },
-      subtract: -> (a,b) { a - b },
-      divide:   -> (a,b) { a / b },
-      multiply: -> (a,b) { a * b },
-      power:    -> (a,b) { a ** b }
+      add:      ->(a,b) { a + b },
+      subtract: ->(a,b) { a - b },
+      divide:   ->(a,b) { a / b },
+      multiply: ->(a,b) { a * b },
+      power:    ->(a,b) { a ** b }
     }
   end
   def method_missing(expression, a, b)
